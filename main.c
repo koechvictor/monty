32eb191c748178748fa442488f698577b4f74fb1
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-06 02:25:22 -0700

    update

--- main.c
+++ main.c
@@ -15,3 +15,34 @@ buf_struct *make_struct(char *argv[])
 
 	return (&a);
 }
+/**
+ * main - Takes argument list and executes file given.
+ * @argc: Argument count.
+ * @argv: List of arguments.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	buf_struct *a;
+
+	a = make_struct(argv);
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	while ((fd = open(argv[1], O_RDONLY)) == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	read(fd, a->read_buff, 4096);
+	close(fd);
+	split_newline(a);
+	exec_loop(a);
+
+	return (0);
+}
