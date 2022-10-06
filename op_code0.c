71654afd84592447097c640474e0b7661339c7fd
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-06 01:45:35 -0700

    update

--- op_code0.c
+++ op_code0.c
@@ -66,3 +66,21 @@ void pint(stack_t **stack, unsigned int line_n)
 	fprintf(stdout, "%d\n", h->n);
 
 }
+
+/**
+ * pop - removes first node of a linked list
+ * @stack: first node of linked list
+ * @line_n: line number
+ */
+void pop(stack_t **stack, unsigned int line_n)
+{
+	stack_t *delete = *stack;
+
+	if (!delete)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_n);
+		exit(EXIT_FAILURE);
+	}
+	*stack = delete->next;
+	free(delete);
+}
