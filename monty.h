5eed6a62315ad0909087e7fc122213c269cb2590
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-06 01:04:30 -0700

    update

--- monty.h
+++ monty.h
@@ -1,6 +1,15 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
