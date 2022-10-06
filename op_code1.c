12177ccb55e2c12653c408575f6e559e7b835cb3
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-06 00:59:40 -0700

    update

--- op_code1.c
+++ op_code1.c
@@ -33,7 +33,8 @@ void sub(stack_t **stack, unsigned int line_n)
 	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_n);
 }
-**
+
+/**
  * _div - adds the top two elements of the stack
  * @stack: start of doubly linked list
  * @line_n: line number
@@ -53,5 +54,28 @@ void _div(stack_t **stack, unsigned int line_n)
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_n);
+}
+
+/**
+ * mod - adds the top two elements of the stack
+ * @stack: start of doubly linked list
+ * @line_n: line number
+ */
+void mod(stack_t **stack, unsigned int line_n)
+{
+	if (!(*stack) || !(*stack)->next)
+	{
+		free_stack(*stack);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_n);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		free_stack(*stack);
+		fprintf(stderr, "L%d: division by zero\n", line_n);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n %= (*stack)->n;
 	pop(stack, line_n);
 }
