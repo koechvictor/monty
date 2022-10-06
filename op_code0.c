e4e9bfc153419ffe1e903301833f95b938730ec9
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-06 01:20:21 -0700

    update

---
+++ op_code0.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+#include <stdlib.h>
+
+/**
+ * push - adds a new node to the beginning of the linked list
+ * @head: beginning of linked list
+ * @n: value of new node
+ * Return: the new node that was added
+ */
+stack_t *push(stack_t **head, int n)
+{
+	stack_t *new;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		if (*head != NULL)
+			free_stack(*head);
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new;
+
+	*head = new;
+
+	return (new);
+}
