6e0fd20b249b17bee048bce0de6adf21d7852191
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-06 00:57:35 -0700

    update

--- op_code1.c
+++ op_code1.c
@@ -33,3 +33,25 @@ void sub(stack_t **stack, unsigned int line_n)
 	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_n);
 }
+**
+ * _div - adds the top two elements of the stack
+ * @stack: start of doubly linked list
+ * @line_n: line number
+ */
+void _div(stack_t **stack, unsigned int line_n)
+{
+	if (!(*stack) || !(*stack)->next)
+	{
+		free_stack(*stack);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_n);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		free_stack(*stack);
+		fprintf(stderr, "L%d: division by zero\n", line_n);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_n);
+}
